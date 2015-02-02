aa7f4a11a32535ba11070d37ddafc4c41eb8caa9
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2015-02-02 12:27:23 -0700

    Initial project setup

--- BlackJack/GameViewController.swift
+++ BlackJack/GameViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ BlackJack/PlayingCard.swift
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.swift
+//  BlackJack
+//
+//  Created by Sameer Totey on 2/2/15.
+//  Copyright (c) 2015 Sameer Totey. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCard: NSObject {
+   
+}
