be3cb69bfde937ac64341b78f80099bce541f3e0
Sameer Totey <sameertotey@hotmail.com>
2019-02-20 16:29:53 -0700

    more changes

--- BlackJack/PlayingCardShoeView.swift
+++ BlackJack/PlayingCardShoeView.swift
@@ -32,24 +32,24 @@ class PlayingCardShoeView: UIView {
     
     func setup () {
         backgroundColor = nil
-        opaque = false
-        contentMode = .Redraw
+        isOpaque = false
+        contentMode = .redraw
 //        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "cardShoeLongPressed:")
 //        self.addGestureRecognizer(longPressGestureRecognizer)
     }
     
     func sendNotification(message: String) {
-        NSNotificationCenter.defaultCenter().postNotificationName(NotificationMessages.setStatus, object: message)
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: NotificationMessages.setStatus), object: message)
     }
     
 
     func cardShoeLongPressed(sender: UILongPressGestureRecognizer) {
-        if sender.state == .Ended {
+        if sender.state == .ended {
             // handling code
-            UIView.transitionWithView(self, duration: 0.25, options: .CurveEaseOut | .TransitionFlipFromBottom, animations: {
+            UIView.transition(with: self, duration: 0.25, options: [.curveEaseOut, .transitionFlipFromBottom], animations: {
                 // send a message to get a new show
                 }, completion: { _ in
-                    self.sendNotification("Getting a new Card Shoe")
+                    self.sendNotification(message: "Getting a new Card Shoe")
                 }
                     )
         }
@@ -58,7 +58,7 @@ class PlayingCardShoeView: UIView {
     // MARK: - initializers
     
     required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        super.init(coder: aDecoder)!
         setup()
     }
     
@@ -82,14 +82,14 @@ class PlayingCardShoeView: UIView {
         static let boxArcRadius: CGFloat = 30.0
     }
     // MARK: - drawing
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         
         let semiColor = UIColor(red: 0.9, green: 0.2, blue: 0.1, alpha: 0.6)
         let sideColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 0.8)
 
         let roundedRect = UIBezierPath(roundedRect:self.bounds, cornerRadius:cornerRadius)
         roundedRect.addClip()
-        UIColor.clearColor().setFill()
+        UIColor.clear.setFill()
         UIRectFill(self.bounds)
         
         // draw the card
@@ -107,50 +107,50 @@ class PlayingCardShoeView: UIView {
         let arcRadius: CGFloat = 20.0
         
         // draw the dummy card
-        let cardRect = CGRectMake(x2, bounds.height - ShoeConstants.cardHeight, ShoeConstants.cardWidth, ShoeConstants.cardHeight)
-        UIImage(named: "cardback-red")?.drawInRect(cardRect)
+        let cardRect = CGRect(x: x2, y: bounds.height - ShoeConstants.cardHeight, width: ShoeConstants.cardWidth, height: ShoeConstants.cardHeight)
+        UIImage(named: "cardback-red")?.draw(in: cardRect)
         
         // draw the shoe opening
         let openingPath = UIBezierPath()
-        openingPath.moveToPoint(CGPointMake(x1, y1))
-        openingPath.addLineToPoint(CGPointMake(x1, y2))
-        openingPath.addLineToPoint(CGPointMake(x5, y2))
-        openingPath.addLineToPoint(CGPointMake(x5, y1))
-        openingPath.addLineToPoint(CGPointMake(x4 + 5.0, y1))
+        openingPath.move(to: CGPoint(x: x1, y: y1))
+        openingPath.addLine(to: CGPoint(x: x1, y: y2))
+        openingPath.addLine(to: CGPoint(x: x5, y: y2))
+        openingPath.addLine(to: CGPoint(x: x5, y: y1))
+        openingPath.addLine(to: CGPoint(x: x4 + 5.0, y: y1))
 
-        openingPath.addArcWithCenter(CGPointMake(45, y1), radius: arcRadius, startAngle: 0, endAngle: CGFloat(M_PI), clockwise: false)
+        openingPath.addArc(withCenter: CGPoint(x: 45, y: y1), radius: arcRadius, startAngle: 0, endAngle: CGFloat(Double.pi), clockwise: false)
 
-        openingPath.closePath()
+        openingPath.close()
         
         semiColor.setFill()
-        UIColor.blackColor().setStroke()
+        UIColor.black.setStroke()
         openingPath.stroke()
         openingPath.fill()
         
 
         // draw the shoe top
         let topRectPath = UIBezierPath()
-        topRectPath.moveToPoint(CGPointMake(x1, y2))
-        topRectPath.addLineToPoint(CGPointMake(x5, y2))
-        topRectPath.addLineToPoint(CGPointMake(bounds.width, y4))
-        topRectPath.addLineToPoint(CGPointMake(x3, y4))
-        topRectPath.closePath()
+        topRectPath.move(to: CGPoint(x: x1, y: y2))
+        topRectPath.addLine(to: CGPoint(x: x5, y: y2))
+        topRectPath.addLine(to: CGPoint(x: bounds.width, y: y4))
+        topRectPath.addLine(to: CGPoint(x: x3, y: y4))
+        topRectPath.close()
         
         sideColor.setFill()
-        UIColor.blackColor().setStroke()
+        UIColor.black.setStroke()
         topRectPath.stroke()
         topRectPath.fill()
         
         // draw the shoe side
         let sideRectPath = UIBezierPath()
-        sideRectPath.moveToPoint(CGPointMake(x5, y1))
-        sideRectPath.addLineToPoint(CGPointMake(bounds.width, y3))
-        sideRectPath.addLineToPoint(CGPointMake(bounds.width, y4))
-        sideRectPath.addLineToPoint(CGPointMake(x5, y2))
-        sideRectPath.closePath()
+        sideRectPath.move(to: CGPoint(x: x5, y: y1))
+        sideRectPath.addLine(to: CGPoint(x: bounds.width, y: y3))
+        sideRectPath.addLine(to: CGPoint(x: bounds.width, y: y4))
+        sideRectPath.addLine(to: CGPoint(x: x5, y: y2))
+        sideRectPath.close()
         
         sideColor.setFill()
-        UIColor.blackColor().setStroke()
+        UIColor.black.setStroke()
         sideRectPath.stroke()
         sideRectPath.fill()
         
@@ -160,17 +160,17 @@ class PlayingCardShoeView: UIView {
     
     func drawText() {
         let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .Center
+        paragraphStyle.alignment = .center
         
-        var textFont = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-        textFont = textFont.fontWithSize(textFont.pointSize * 0.6)
+        var textFont = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body)
+        textFont = textFont.withSize(textFont.pointSize * 0.6)
         
-        let textText = NSAttributedString(string: "\(numDecks) Deck", attributes:[NSFontAttributeName : textFont, NSParagraphStyleAttributeName : paragraphStyle])
+        let textText = NSAttributedString(string: "\(numDecks) Deck", attributes:[NSAttributedString.Key.font : textFont, NSAttributedString.Key.paragraphStyle : paragraphStyle])
         
         var textBounds = CGRect()
-        textBounds.origin = CGPointMake(6, 6)
+        textBounds.origin = CGPoint(x: 6, y: 6)
         textBounds.size = textText.size()
-        textText.drawInRect(textBounds)
+        textText.draw(in: textBounds)
         
     }
 
